40d5a2d5e717b86f82b6ebf29a6af116b2470339
Clayton Oliveira <clayton.will08@gmail.com>
2025-08-11 22:21:35 -0300

    - Add ViewModels

---
+++ Salvation/ViewModels/FilmeViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Salvation.ViewModels
+{
+    public class FilmeViewModel
+    {
+        public int IdFilme { get; set; }
+        public string TituloFilme { get; set; }
+        public string ProdutoraFilme { get; set; }
+        public string? UrlImagem { get; set; }
+        public IFormFile? ImagemUpload { get; set; }
+        public int ClassificacaoId { get; set; }
+        public int GeneroId { get; set; }
+
+        //Coleção para popular o dropdown
+        public IEnumerable<SelectListItem>? Classificacoes { get; set; }
+        public IEnumerable<SelectListItem>? Generos { get; set; }
+
+    }
+}
---
+++ Salvation/ViewModels/UsuarioViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Salvation.ViewModels
+{
+    public class UsuarioViewModel
+    {
+        public int IdUsuario { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public int TipoUsuarioId { get; set; }
+
+        //Coleção para popular o dropdown
+        public IEnumerable<SelectListItem>? TipoUsuario { get; set; }
+    }
+}
