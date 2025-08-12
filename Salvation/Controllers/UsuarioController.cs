08d7db73d11ad8cdee26f1bafcc448b6da78c926
Clayton Oliveira <clayton.will08@gmail.com>
2025-08-11 22:22:15 -0300

    - Add Controllers

---
+++ Salvation/Controllers/UsuarioController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Salvation.Interfaces;
+
+namespace Salvation.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        private readonly IUsuarioRepository _usuarioRepository;
+
+        public UsuarioController(IUsuarioRepository usuarioRepository)
+        {
+            _usuarioRepository = usuarioRepository;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
